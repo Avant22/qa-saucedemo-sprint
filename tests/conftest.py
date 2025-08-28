f7bfbb3777739adac5ac84bf15700543b4fceca3
ThePurplePen <ThePurplePen@ThePurplePen.homenet.telecomitalia.it>
2025-08-28 15:00:44 +0200

    Enable screenshots and Playwright traces on failures

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,14 @@ def app_url():
     return os.environ.get("BASE_URL", "https://www.saucedemo.com")
 
 @pytest.fixture(scope="function")
-def page_context(page, app_url):
+def page_context(context, app_url, request):
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+    page = context.new_page()
     page.set_default_timeout(10_000)
     page.goto(app_url)
     yield page
+    trace_file = os.path.join("artifacts", f"{request.node.name}-trace.zip")
+    context.tracing.stop(path=trace_file)
+    if request.node.rep_call.failed:
+        screenshot_path = os.path.join("artifacts", f"{request.node.name}.png")
+        page.screenshot(path=screenshot_path, full_page=True)
